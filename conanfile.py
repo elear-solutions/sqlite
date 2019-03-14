836a138a4e5b551632ed2093c5711ca86d495360
deekshith <venkatadeekshith@elear.solutions>
2019-03-14 21:26:43 +0530

    Provides conan file to build artifacts

---
+++ conanfile.py
@@ -0,0 +1,30 @@
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
+
+
+class SqlitelibConan(ConanFile):
+    name = "sqlite"
+    version = "0.1"
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Elearcommonlib here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "make"
+
+
+    def build(self):
+        autotools = AutoToolsBuildEnvironment(self)
+        self.run("cd .. && autoreconf -fsi ")
+        autotools.configure(configure_dir="..",args=["--prefix=${PWD}"])
+        autotools.make()
+        autotools.install()
+    def package(self):
+        self.copy("*.h", dst="include", src="include")
+        self.copy("*", dst="lib", src="lib")
+
+    def package_info(self):
+        self.cpp_info.libs = ["sqlite"]
+
