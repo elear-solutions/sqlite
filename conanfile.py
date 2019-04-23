09d046874c325779cd574e318d0a91836647fb66
deekshith <venkatadeekshith@elear.solutions>
2019-04-23 18:47:05 +0530

    Eliminate indentation errors

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ class SqlitelibConan(ConanFile):
     license = "<Put the package license here>"
     author = "<Put your name here> <And your email here>"
     url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Elearcommonlib here>"
+    description = "<Description of Sqlitelib here>"
     topics = ("<Put some tag here>", "<here>", "<and here>")
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
